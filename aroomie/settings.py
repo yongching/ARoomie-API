7262a51d69eab2da11f0897e6f74a8154ccc6bb3
yongching <yctee_04@outlook.com>
2016-12-13 14:48:09 +0800

    Fixed typo for BASE_DIR

--- aroomie/settings.py
+++ aroomie/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 LOGIN_REDIRECT_URL = '/'
