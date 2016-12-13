f315b148f3e48c5a6e7355ecd96309c6a689cf10
yongching <yctee_04@outlook.com>
2016-12-13 15:57:51 +0800

    Edited home.html

--- aroomie/settings.py
+++ aroomie/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+# Testing
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'aroomieapp/static'),
 )
