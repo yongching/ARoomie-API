e10baa9cf5802ee275a389195eedfc132c9d6945
yongching <yctee_04@outlook.com>
2016-12-13 15:06:13 +0800

    Update settings

--- aroomie/development.py
+++ aroomie/development.py
@@ -3,3 +3,6 @@ from .settings import *
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
--- aroomie/production.py
+++ aroomie/production.py
@@ -3,3 +3,11 @@ from .settings import *
 DEBUG = False
 
 ALLOWED_HOSTS = ['aroomie.herokuapp.com']
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
