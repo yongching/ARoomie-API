a413f054fec0360841f48c859e3e69f6c8b56265
yongching <yctee_04@outlook.com>
2016-12-13 15:37:00 +0800

    Edited STATICFILES_DIRS

--- aroomie/settings.py
+++ aroomie/settings.py
@@ -14,7 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -118,16 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'aroomieapp/static'),
 )
-
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/'
 
--- aroomieapp/views.py
+++ aroomieapp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.conf import settings
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    return render(request, 'home.html', {
+        "BASE_DIR": settings.BASE_DIR,
+        "STATICFILES_DIRS": settings.STATICFILES_DIRS
+    })
