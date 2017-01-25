d8d4e084bd067b1ff1e886bd65d3fdece5b1de4b
yongching <reachyongching@gmail.com>
2017-01-25 18:54:15 +0800

    Set default lifestyle info when user register

--- aroomieapp/social_auth_pipeline.py
+++ aroomieapp/social_auth_pipeline.py
@@ -6,6 +6,7 @@ def extend_user_profile(backend, user, request, response, *args, **kwargs):
         gender = response['gender']
         age_min = response['age_range']['min'] if response['age_range'].get('min') else 0
         age_max = response['age_range']['max'] if response['age_range'].get('max') else 0
+        lifestyle_info = "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0"
 
         if not Profile.objects.filter(user_id=user.id):
-            Profile.objects.create(user_id=user.id, age_min=age_min, age_max=age_max, gender=gender, avatar=avatar)
+            Profile.objects.create(user_id=user.id, age_min=age_min, age_max=age_max, gender=gender, avatar=avatar, lifestyle_info=lifestyle_info)
