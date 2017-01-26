094a217c88e0cb02676ecaa1367d46b9567ff7e1
yongching <reachyongching@gmail.com>
2017-01-27 05:10:37 +0800

    Testing

--- aroomie/settings.py
+++ aroomie/settings.py
@@ -179,5 +179,6 @@ SOCIAL_AUTH_PIPELINE = (
 
 # Push notification
 PUSH_NOTIFICATIONS_SETTINGS = {
-    "APNS_CERTIFICATE": os.path.join(BASE_DIR, 'aroomieapp/certificate.pem'),
+    # "APNS_CERTIFICATE": os.path.join(BASE_DIR, 'aroomieapp/certificate.pem'),
+    "APNS_CERTIFICATE": os.environ['APNS_CERTIFICATE'],
 }
